156ac0ab51926ee2443403a8c2a1fc55d71ddd50
DK <jenna_kim1@hotmail.com>
2019-10-26 14:42:32 +0200

    added firebase public key in env & prettier

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAOnBz1xpbkvFW_Q5eg7r9eUKK2UtZJmRE',
+    authDomain: 'job-hunting-test.firebaseapp.com',
+    databaseURL: 'https://job-hunting-test.firebaseio.com',
+    projectId: 'job-hunting-test',
+    storageBucket: 'job-hunting-test.appspot.com',
+    messagingSenderId: '886554015134',
+    appId: '1:886554015134:web:8f6d5f1a5d0056594c928f',
+    measurementId: 'G-6D4S2Z248L'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAOnBz1xpbkvFW_Q5eg7r9eUKK2UtZJmRE',
+    authDomain: 'job-hunting-test.firebaseapp.com',
+    databaseURL: 'https://job-hunting-test.firebaseio.com',
+    projectId: 'job-hunting-test',
+    storageBucket: 'job-hunting-test.appspot.com',
+    messagingSenderId: '886554015134',
+    appId: '1:886554015134:web:8f6d5f1a5d0056594c928f',
+    measurementId: 'G-6D4S2Z248L'
+  }
 };
 
 /*
