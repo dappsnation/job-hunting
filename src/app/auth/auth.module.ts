b71d53b031aa2cc5dddaa7e59f0e4cb65a557c23
DK <jenna_kim1@hotmail.com>
2019-10-27 16:44:03 +0100

    generated auth module

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SigninComponent } from './signin/signin.component';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class AuthModule { }
