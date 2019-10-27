88649258c907f7d2e0f019ec009abeda7a188306
DK <jenna_kim1@hotmail.com>
2019-10-27 16:49:15 +0100

    added router, angularfire,firestore,fireauth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,34 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
 import { environment } from '../environments/environment';
+import { RouterModule } from '@angular/router';
+import { SigninComponent } from './auth/signin/signin.component';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireModule } from '@angular/fire';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, SigninComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    environment.production ? [] : AkitaNgDevtools.forRoot(),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    // environment.production ? [] : AkitaNgDevtools.forRoot(),
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: 'signin',
+        pathMatch: 'full'
+      },
+      {
+        path: 'signin',
+        component: SigninComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
