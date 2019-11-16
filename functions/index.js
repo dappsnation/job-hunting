d60f44bb3881c722bcdb4a1f9b8a200998f04bcf
Campalo <25295489+Campalo@users.noreply.github.com>
2019-11-16 22:26:19 +0100

    Clear the functions

--- functions/index.js
+++ functions/index.js
@@ -17,32 +17,24 @@ const mailTransport = nodemailer.createTransport({
 
 const APP_NAME = 'Job hunting emails';
 
-// [START sendWelcomeEmail]
-/**
- * Sends a welcome email to new user.
- */
+// [START sendWelcomeEmail to a new user]
 // [START onCreateTrigger]
 exports.sendWelcomeEmail = functions.auth.user().onCreate(user => {
   // [END onCreateTrigger]
   // [START eventAttributes]
-  const email = user.email; // The email of the user.
-  const displayName = user.displayName; // The display name of the user.
+  const email = user.email;
+  const displayName = user.displayName;
   // [END eventAttributes]
-
   return sendWelcomeEmail(email, displayName);
 });
 // [END sendWelcomeEmail]
 
-// [START sendByeEmail]
-/**
- * Send an account deleted email confirmation to users who delete their accounts.
- */
+// [START sendByeEmail to users who delete their accounts]
 // [START onDeleteTrigger]
 exports.sendByeEmail = functions.auth.user().onDelete(user => {
   // [END onDeleteTrigger]
   const email = user.email;
   const displayName = user.displayName;
-
   return sendGoodbyeEmail(email, displayName);
 });
 // [END sendByeEmail]
@@ -53,14 +45,6 @@ async function sendWelcomeEmail(email, displayName) {
     from: `${APP_NAME} <noreply@firebase.com>`,
     to: email
   };
-
-  // The user subscribed to the newsletter.
-  mailOptions.subject = `Welcome to ${APP_NAME}!`;
-  mailOptions.text = `Hey ${displayName ||
-    ''}! Welcome to ${APP_NAME}. I hope you will enjoy our service.`;
-  await mailTransport.sendMail(mailOptions);
-  console.log('New welcome email sent to:', email);
-  return null;
 }
 
 // Sends a goodbye email to the given user.
@@ -69,12 +53,8 @@ async function sendGoodbyeEmail(email, displayName) {
     from: `${APP_NAME} <noreply@firebase.com>`,
     to: email
   };
-
-  // The user unsubscribed to the newsletter.
-  mailOptions.subject = `Bye!`;
-  mailOptions.text = `Hey ${displayName ||
-    ''}!, We confirm that we have deleted your ${APP_NAME} account.`;
-  await mailTransport.sendMail(mailOptions);
-  console.log('Account deletion confirmation email sent to:', email);
-  return null;
 }
+
+// Credentials:
+// jubhunting.test@gmail.com
+// jobhuntingberlin19
